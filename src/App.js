47abebfef85d66ed5b4359ea3067a38d568a0a0d
James <jimblackwood3@gmail.com>
2018-07-10 06:43:06 -0700

    query arxiv api for papers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Papers from './components/Papers'
+import './App.css'; 
 
-class App extends Component {
+class App extends Component { 
+  constructor() {
+    super()
+    this.state = {
+      papers: []
+    }
+  }
+
+  
+
+  componentWillMount() {
+    var convert = require('xml-js')
+    function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+  }
+
+    let url = "http://export.arxiv.org/api/query?search_query=electron&sortBy=lastUpdatedDate&sortOrder=descending&max_results=10"
+    httpGetAsync(url, function(response) {
+      let json_papers = convert.xml2json(response)
+      console.log(json_papers)
+    })
+    
+    
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Cite Opt</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App">
+         <Papers />
+       </p>
       </div>
     );
   }
---
+++ src/components/Papers.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Papers extends Component {
+	render() {
+    return (
+      <div className="Papers">
+        Papers from Arxiv.
+      </div>
+    );
+	}
+}
+
+export default Papers;
